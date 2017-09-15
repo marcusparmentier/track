a9f957bffe8390272a812cb3f7eacee90ed4b255
Marcus Parmentier <marcusjparmentier@gmail.com>
2017-09-15 16:24:19 -0700

    Make scripts.js logic funtional, add css to highlight the suggested tracks, and clean some html specifically the bottom cols.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+var add = function(q1, q2, q3, q4, q5) {
+  return q1 + q2 + q3 + q4 + q5;
+};
+
 $(function() {
   $("form#formInput").submit(function(event) {
     var q1 = parseInt($("select#q1sel").val());
@@ -6,25 +10,36 @@ $(function() {
     var q4 = parseInt($("select#q4sel").val());
     var q5 = parseInt($("select#q5sel").val());
     var result = add(q1, q2, q3, q4, q5);
-    var result = parseInt($(result).val());
+
 
         if (result >= 26) {
-          $("add class fix and show");
-        }
-        else if (result < 26 && >= 21) {
-          $("add class fix and show");
-        }
-        else if (result < 21 && >= 16) {
-          $("add class fix and show");
-        }
-        else if (result < 16 && >= 11) {
-          $("add class fix and show");
-        }
-        else if (result < 11 && >= 6) {
-          $("add class fix and show");
-        }
-        else {
-          alert("Please select values for all questions.");
+            $("div").removeClass("sugg1 sugg2 sugg3");
+            $('#netC').addClass("sugg1");
+            $('#phpD').addClass("sugg2");
+            $('#javaA').addClass("sugg3");
+        } else if (result < 26 && result >= 21) {
+            $("div").removeClass("sugg1 sugg2 sugg3");
+            $('#phpD').addClass("sugg1");
+            $('#netC').addClass("sugg2");
+            $('#javaA').addClass("sugg3");
+        } else if (result < 21 && result >= 16) {
+            $("div").removeClass("sugg1 sugg2 sugg3");
+            $('#javaA').addClass("sugg1");
+            $('#phpD').addClass("sugg2");
+            $('#rubyR').addClass("sugg3");
+        } else if (result < 16 && result >= 11) {
+            $("div").removeClass("sugg1 sugg2 sugg3");
+            $('#rubyR').addClass("sugg1");
+            $('#javaA').addClass("sugg2");
+            $('#cssD').addClass("sugg3");
+        } else if (result < 11 && result >= 5) {
+            $("div").removeClass("sugg1 sugg2 sugg3");
+            $('#cssD').addClass("sugg1");
+            $('#rubyR').addClass("sugg2");
+            $('#javaA').addClass("sugg3");
+        } else {
+            $("div").removeClass("sugg1 sugg2 sugg3");
+            alert("Please select values for all questions.");
         }
 
 
